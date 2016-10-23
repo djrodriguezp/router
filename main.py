334cf5b1b967d82c6630ba3b4b3545388487c114
Alejandro <alex.at019@gmail.com>
2016-10-23 15:44:30 -0600

    Fixing shiat

--- main.py
+++ main.py
@@ -15,11 +15,6 @@ def start():
     routingModule.run()
 
 start()
-"""
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.connect(("192.168.2.153",9080))
-MessageSender(say_my_name).send(s, "CUALQUIER", ["MIERDA 1", "MIERDA 2", "MIERDA 3"])
-s.close()
-"""
+
 
 
