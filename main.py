78895e4665f4fcc906d411107e51e6527872326e
Paolo Solombrino <paolo.solombrino@xoom.com>
2016-10-22 21:55:02 -0700

    routing

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
-# coding=utf-8
-import socket
-
-from MessageSender import MessageSender
-from TxChannel import TxChannel
-ROUTING_PORT = 9080
-say_my_name = "0xBEEFBABE"
-
-def read_conf_file(filename):
-    f = open(filename, "r")
-    for line_no, line in enumerate(f):
-        node = line.rstrip().split(";")
-        if len(node) != 3:
-            raise AssertionError("Expected 3 values delimited by ; at line " + str(line_no + 1) + " file: " + filename)
-        name = node[0]
-        ip = node[2]
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            s.connect((ip, ROUTING_PORT))
-        except Exception as e:
-            print "No se pudo establecer conexión tcp con " + name + "(" + ip + ")"
-            print(e)
-        else:
-            TxChannel(name, MessageSender(say_my_name), s).start()
+from Routing import Routing
 
 
 
-read_conf_file("neighbors.conf")
+def start():
+    Routing().run()
 
-
+start()
 """
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #s.connect(("192.168.2.153",9080))
