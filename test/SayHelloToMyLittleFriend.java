eee89b5f5a753ed225e17420992aeac4d64c4a05
vash <team161419@gmail.com>
2016-10-23 18:20:00 -0600

    test

--- test/SayHelloToMyLittleFriend.java
+++ test/SayHelloToMyLittleFriend.java
@@ -5,14 +5,20 @@ class SayHelloToMyLittleFriend
 {
  public static void main(String args[]) throws Exception
  {
-  String message = "From:"+args[0]+"\n"+args[1]+":Y\nMsg:"+args[2]+"\nEOF";
-  System.out.println(message);
-  String response = "";
-  BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-  Socket clientSocket = new Socket("192.168.1.18", 1981);
-  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-  BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-  outToServer.write(message.getBytes());
-  clientSocket.close();
+    if(args.length == 4)
+    {
+        String message = "From:"+args[1]+"\nTo:"+args[2]+"\nMsg:"+args[2]+"\nEOF";
+        String response = "";
+        BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+        Socket clientSocket = new Socket(args[0], 1981);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        outToServer.write(message.getBytes());
+        clientSocket.close();
+    }
+    else
+    {
+        System.out.println("Uso: java SayHelloToMyLittleFriend <ip> <from> <to> <mensaje>");
+    }
  }
 }
