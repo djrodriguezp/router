f997d55cab7c6da61adf6ff6ebb3a5b2cb4080bf
Alejandro <alex.at019@gmail.com>
2016-10-26 02:30:37 -0600

    Fix ip address

--- RoutingLobby.py
+++ RoutingLobby.py
@@ -44,8 +44,8 @@ class RoutingLobby(Thread):
                     if neighbor is None:
                         newSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                         newSock.settimeout(2)
-                        newSock.connect((addr, Routing.Routing.INSTANCE.ROUTING_PORT))
-                        node = Routing.Routing.INSTANCE.makeNode(msg.origin, 99, addr)
+                        newSock.connect((addr[0], Routing.Routing.INSTANCE.ROUTING_PORT))
+                        node = Routing.Routing.INSTANCE.makeNode(msg.origin, 99, addr[0])
                         Routing.Routing.INSTANCE.addNeighbor(node, newSock, False)
                         node.tx.start()
                     """
